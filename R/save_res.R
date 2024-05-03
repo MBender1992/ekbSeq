034207020dfb85845952ff902813c0a9e51344b2
MBender1992 <mbender1992@web.de>
2024-05-03 09:15:44 +0200

    update_save_res

--- R/save_res.R
+++ R/save_res.R
@@ -2,18 +2,18 @@
 #'
 #' @param save logical indicating whether workspace and significant results table should be saved. Default is TRUE.
 #' @param results data.frame containing results with differentially expressed genes.
-#' @param path path where data should be saved. Must end with "<<filename>>.csv".
-#' @param file.name name of the exported csv and rdata file.
+#' @param results.name name of csv file where differential expression results are stored.
+#' @param rdata.name name of the exported rdata file.
 #' @param rm.batch second factor of a design formula. If present the batch effect introduced by this covariate is removed and the corresponding
 #' files are saved with the suffix "adjusted".
 #' @export
 
-save_res <- function(save = TRUE, results = NULL, path = NULL, file.name, rm.batch = NULL){
+save_res <- function(save = TRUE, results = NULL, results.name = NULL, rdata.name, rm.batch = NULL){
   if(save == TRUE){
     warning("The current DSC_NGS file has been overwritten. Please check carefully if results are still valid.")
-    if(!is.null(rm.batch)) save.image(file = paste(file.name, "_adjusted.RData", sep = "")) else save.image(file = paste(file.name, ".RData", sep = ""))
-    if(!is.null(path) & !is.null(results)){
-      if(!is.null(rm.batch)) write.csv(results, paste(path, "_adjusted.csv", sep = "")) else write.csv(results, paste(path, ".csv", sep = ""))
+    if(!is.null(rm.batch)) save.image(file = paste(rdata.name, "_adjusted.RData", sep = "")) else save.image(file = paste(rdata.name, ".RData", sep = ""))
+    if(!is.null(results.name) & !is.null(results)){
+      if(!is.null(rm.batch)) write.csv(results, paste(results.name, "_adjusted.csv", sep = "")) else write.csv(results, paste(results.name, ".csv", sep = ""))
     } else {
       warning("No results file or path has been provided. Results table will not be saved.")
     }
